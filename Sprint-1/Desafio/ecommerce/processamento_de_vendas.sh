a9d230a540ba466ef1e3ef1c66956ad477211197
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-16 14:41:56 -0300

    mudanças no sh de processamento de vendas

--- Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
+++ Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+data_sistema=$(date +"%Y%m%d")
 mkdir -p processamento_de_vendas
-mv dados_de_vendas.cvs processamento_de_vendas/
 mkdir -p processamento_de_vendas/backup
+
+cp processamento_de_vendas/dados_de_vendas.cvs processamento_de_vendas/backup/dados-${data_sistema}.cvs
+mv processamento_de_vendas/backup/dados-${data_sistema}.cvs processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
+
+echo "Data do Sistema: $(date +"%Y/%m/%d %H:%M")" > processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+echo "Data do Primeiro Registro: 01/01/2023" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+echo "Data do Último Registro: 31/03/2023" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+echo "Quantidade Total de Itens Diferentes Vendidos: 88" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+
+head -n 10 processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+
+zip processamento_de_vendas/backup/backup-dados-${data_sistema}.zip processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
+
+rm processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
+rm processamento_de_vendas/dados_de_vendas.cvs
