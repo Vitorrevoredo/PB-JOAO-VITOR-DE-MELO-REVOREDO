0ed70a795ca96da495c7c22f8f84a97541e454ee
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-16 19:14:36 -0300

    Ajustando modificações no arquivo .sh, já está gerando o relatorio

--- Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
+++ Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 data_sistema=$(date +"%Y%m%d")
-mkdir -p processamento_de_vendas
-mkdir -p processamento_de_vendas/backup
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce
+mkdir -p vendas/backup
 
-cp processamento_de_vendas/dados_de_vendas.cvs processamento_de_vendas/backup/dados-${data_sistema}.cvs
-mv processamento_de_vendas/backup/dados-${data_sistema}.cvs processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
+cp ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/dados_de_vendas.csv ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+mv dados_de_vendas.csv backup-dados-${data_sistema}.csv
 
-echo "Data do Sistema: $(date +"%Y/%m/%d %H:%M")" > processamento_de_vendas/backup/relatorio.txt-${data_sistema}
-echo "Data do Primeiro Registro: 01/01/2023" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
-echo "Data do Último Registro: 31/03/2023" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
-echo "Quantidade Total de Itens Diferentes Vendidos: 88" >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+echo relatorio.txt-${data_sistema}
+echo "Data do Sistema: $(date +"%Y/%m/%d %H:%M")" >> relatorio.txt-${data_sistema}
+quantidade_linhas= wc -l backup-dados-${data_sistema}.csv
+echo "Data do Primeiro Registro: 01/01/2023" >> relatorio.txt-${data_sistema}
+echo "Data do Último Registro: 31/01/2023" >> relatorio.txt-${data_sistema}
+echo "Quantidade Total de Itens Diferentes Vendidos: -${quantidade_linhas}" >> relatorio.txt-${data_sistema}
 
-head -n 10 processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs >> processamento_de_vendas/backup/relatorio.txt-${data_sistema}
+head -n 11  backup-dados-${data_sistema}.csv >> relatorio.txt-${data_sistema}
+zip backup-dados-${data_sistema}.zip backup-dados-${data_sistema}.csv
 
-zip processamento_de_vendas/backup/backup-dados-${data_sistema}.zip processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
-
-rm processamento_de_vendas/backup/backup-dados-${data_sistema}.cvs
-rm processamento_de_vendas/dados_de_vendas.cvs
+rm backup-dados-${data_sistema}.csv
