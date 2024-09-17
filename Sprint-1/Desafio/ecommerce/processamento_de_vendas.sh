c0d8ce5f0c8dc3553fb4e13e842acab6e83b9474
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-17 11:56:19 -0300

    Criação do arquivo zip adicionada

--- Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
+++ Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-data_sistema=$(date +"%Y%m%d")
 cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce
-
+data_sistema=$(date +"%Y%m%d")
 quantidade_produtos=$(awk -F ',' 'END {print $1}' dados_de_vendas.csv)
 ultima_data=$(awk -F ',' 'END {print $5}' dados_de_vendas.csv)
 
-mkdir -p vendas/backup
-
+cp dados_de_vendas.csv ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas
 cp dados_de_vendas.csv ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
 cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
 mv dados_de_vendas.csv backup-dados-${data_sistema}.csv
@@ -18,5 +16,9 @@ echo "Data do Último Registro: ${ultima_data}" >> relatorio.txt-${data_sistema}
 echo "Quantidade Total de Itens Diferentes Vendidos: ${quantidade_produtos}" >> relatorio.txt-${data_sistema}
 head -n 11 backup-dados-${data_sistema}.csv >> relatorio.txt-${data_sistema}
 
-zip backup-dados-${data_sistema}.zip backup-dados-${data_sistema}.csv
-rm backup-dados-${data_sistema}.csv
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+zip -r backup-dados-${data_sistema}.zip backup-dados-${data_sistema}.csv
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas
+rm -rf dados_de_vendas.csv 
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+rm -rf backup-dados-${data_sistema}.csv
