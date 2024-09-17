7ab817cd0323d09eb299043e63e027a3deb36347
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-16 21:42:26 -0300

    Modificações na estruturação dos arquivos

--- Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
+++ Sprint-1/Desafio/ecommerce/processamento_de_vendas.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 data_sistema=$(date +"%Y%m%d")
-
 cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce
 
 quantidade_produtos=$(awk -F ',' 'END {print $1}' dados_de_vendas.csv)
 ultima_data=$(awk -F ',' 'END {print $5}' dados_de_vendas.csv)
 
-mkdir -p vendas
-cd vendas
-mkdir backup
+mkdir -p vendas/backup
 
-cp ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/dados_de_vendas.csv ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+cp dados_de_vendas.csv ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
 cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
 mv dados_de_vendas.csv backup-dados-${data_sistema}.csv
 
@@ -19,8 +16,7 @@ echo "Data do Sistema: $(date +"%Y/%m/%d %H:%M")" >> relatorio.txt-${data_sistem
 echo "Data do Primeiro Registro: 01/01/2023" >> relatorio.txt-${data_sistema}
 echo "Data do Último Registro: ${ultima_data}" >> relatorio.txt-${data_sistema}
 echo "Quantidade Total de Itens Diferentes Vendidos: ${quantidade_produtos}" >> relatorio.txt-${data_sistema}
-
 head -n 11 backup-dados-${data_sistema}.csv >> relatorio.txt-${data_sistema}
-zip backup-dados-${data_sistema}.zip backup-dados-${data_sistema}.csv
 
+zip backup-dados-${data_sistema}.zip backup-dados-${data_sistema}.csv
 rm backup-dados-${data_sistema}.csv
