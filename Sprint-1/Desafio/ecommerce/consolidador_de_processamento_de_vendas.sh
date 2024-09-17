d40040268f52747fccd058090912281bd931877a
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-17 14:48:15 -0300

    Consolidador de arquivos criado

---
+++ Sprint-1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce
+echo relatorio_final.txt
+cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+cat  relatorio.txt-* >> ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/relatorio_final.txt
