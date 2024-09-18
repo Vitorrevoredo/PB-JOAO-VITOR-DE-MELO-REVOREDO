ae9e6248dcca0a2f6ec50ca9d646a5e488c24476
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-09-18 14:58:23 -0300

    Mudanças nas variaveis que armazenam a primeira data e ultima ordenada

--- Sprint-1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
+++ Sprint-1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce
-echo relatorio_final.txt
 cd ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/vendas/backup
+echo relatorio_final.txt
 cat relatorio.txt-* >> ~/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-1/Desafio/ecommerce/relatorio_final.txt
