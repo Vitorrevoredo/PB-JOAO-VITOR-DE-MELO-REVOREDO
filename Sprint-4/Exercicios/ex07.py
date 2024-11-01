44191d6689443aa6eb9370b7442a5f6cf0ba7ac3
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-11-01 14:35:17 -0300

    Ajuste exercicios

--- Sprint-4/Exercicios/ex07.py
+++ Sprint-4/Exercicios/ex07.py
@@ -1,5 +1,9 @@
 def pares_ate(n:int):
-    return (i for i in range(2, n + 1, 2)) # retorna valores pares, utilizando o passo =2 que garante os valores pares
+    for i in range(2, n+1):
+        if i % 2 ==0:
+            yield i
+print(list(pares_ate(10)))
+# retorna valores pares, utilizando o passo =2 que garante os valores pares
 # exemplo do uso do codigo com o numero 10, retornando uma lista dos valores pares
 print(list(pares_ate(10)))
 #resultado esperado:
