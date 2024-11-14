c040d5c513ae2586062372118c30cb6423769d6f
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2024-11-14 10:41:30 -0300

    modificações nos arquivos

--- Sprint-5/Desafio/processamento_pandas.py
+++ Sprint-5/Desafio/processamento_pandas.py
@@ -2,45 +2,35 @@ import pandas as pd
 import boto3
 from io import StringIO
 
-# Nome do bucket e arquivo no S3
+# Configurações
 bucket_name = 'bucket-desafio-vitor'
 file_name = 'VendasTesouroDireto.csv'
+output_file = 'C:/Users/Vitor/Documents/trabalho/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-5/Desafio/VendasTesouroDireto_modificado.csv'
 
+# Conectando ao S3 e ler o arquivo
 s3 = boto3.client('s3')
-obj = s3.get_object(Bucket=bucket_name, Key=file_name)
-csv_data = obj['Body'].read().decode('utf-8')
+csv_data = s3.get_object(Bucket=bucket_name, Key=file_name)['Body'].read().decode('utf-8')
 df = pd.read_csv(StringIO(csv_data), sep=';')
 
-# 4.1 Filtrar dados com dois operadores lógicos
+# 4.1 Filtrando dados
 df_filtrado = df[(df['PU'] > 1000) & (df['Quantidade'] < 50)]
 print("\nFiltrando dados onde PU > 1000 e Quantidade < 50:\n", df_filtrado)
 
-# 4.2 Funções de agregação
-media_pu = df['PU'].mean()
-soma_valor = df['Valor'].sum()
-print(f"\nMédia do PU: {media_pu}")
-print(f"Soma do Valor: {soma_valor}")
+# 4.2 Funções de Agregações (Soma e Média) dos valores
+print(f"\nMédia do PU: {df['PU'].mean()}")
+print(f"Soma do Valor: {df['Valor'].sum()}")
 
 # 4.3 Função Condicional
 df['Categoria Valor'] = df['Valor'].apply(lambda x: 'Alto' if x > 100000 else 'Baixo')
-print("\nAdicionando categoria 'Alto' ou 'Baixo' com base no Valor:\n", df[['Valor', 'Categoria Valor']])
 
-# 4.4 Função de Conversão
+# 4.4 e 4.5 Conversão e extração de ano
 df['Data Venda'] = pd.to_datetime(df['Data Venda'], format='%d/%m/%Y')
-print("\nConvertendo a coluna 'Data Venda' para o formato datetime:\n", df[['Data Venda']].head())
-
-# 4.5 Função de Data
 df['Ano Venda'] = df['Data Venda'].dt.year
-print("\nAdicionando coluna 'Ano Venda' com o ano de cada venda:\n", df[['Data Venda', 'Ano Venda']].head())
 
 # 4.6 Função de String
 df['Tipo Titulo'] = df['Tipo Titulo'].str.upper()
-print("\nConvertendo 'Tipo Titulo' para maiúsculas:\n", df[['Tipo Titulo']].head())
 
-# Salvar o resultado em um novo CSV
-output_csv = 'C:\Users\Vitor\Documents\trabalho\PB-JOAO-VITOR-DE-MELO-REVOREDO\Sprint-5\Desafio\VendasTesouroDireto_modificado.csv'
-df.to_csv(output_csv, index=False, sep=';')
-
-# Carregar o arquivo modificado para o S3
-s3.upload_file(output_csv, bucket_name, 'VendasTesouroDireto_modificado.csv')
+# Salvar e carregar para o S3
+df.to_csv(output_file, index=False, sep=';')
+s3.upload_file(output_file, bucket_name, 'VendasTesouroDireto_modificado.csv')
 print(f'Arquivo modificado carregado com sucesso para o bucket {bucket_name}.')
--- Sprint-5/Desafio/s3_upload_script.py
+++ Sprint-5/Desafio/s3_upload_script.py
@@ -1,14 +1,13 @@
 import boto3
 
 # Configuração do cliente S3 com a região apropriada
-s3 = boto3.client('s3', region_name='us-east-1')
+client = boto3.client('s3', region_name='us-east-1')
 
-# Definindo o nome do bucket e o nome do arquivo
+# Definindo o nome do bucket e o caminho do arquivo local
 bucket_name = 'bucket-desafio-vitor'
 file_name = 'VendasTesouroDireto.csv'
 file_path = 'C:/Users/Vitor/Documents/trabalho/PB-JOAO-VITOR-DE-MELO-REVOREDO/Sprint-5/Desafio/VendasTesouroDireto.csv'
 
-# Tentando carregar o arquivo para o S3
-s3.upload_file(file_path, bucket_name, file_name)
+# Carregando o arquivo para o S3
+client.upload_file(file_path, bucket_name, file_name)
 print(f'Arquivo {file_name} carregado com sucesso para o bucket {bucket_name}.')
-    
