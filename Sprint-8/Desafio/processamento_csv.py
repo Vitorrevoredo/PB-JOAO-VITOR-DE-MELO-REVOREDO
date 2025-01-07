c2c39d0b74e6400b89c18924848749f0302310b8
Vitor Revoredo <joaovitorderevoredo@gmail.com>
2025-01-07 16:23:57 -0300

    Imagens evidencia desafio sprint 9

--- Sprint-8/Desafio/processamento_csv.py
+++ Sprint-8/Desafio/processamento_csv.py
@@ -39,5 +39,20 @@ if "genero" in df.columns:
     # Validar o gênero (excluir registros com gênero nulo ou inválido)
     df = df.filter(col("genero").isNotNull() & (col("genero") != ""))
 
-# Persistir em formato Parquet na camada Trusted
-df.write.mode("overwrite").parquet(TRUSTED_PATH)
+# Adiciona as colunas de particionamento: ano, mês e dia (data atual)
+current_date_str = datetime.now().strftime("%Y-%m-%d")
+year_val = current_date_str.split("-")[0]
+month_val = current_date_str.split("-")[1]
+day_val = current_date_str.split("-")[2]
+
+# Adiciona as colunas de particionamento
+df_with_partitions = movie_genre_combined_df.withColumn("year", col("release_date").substr(1, 4)) \
+                                           .withColumn("month", col("release_date").substr(6, 2)) \
+                                           .withColumn("day", col("release_date").substr(9, 2))
+
+# Modificação para o caminho correto, conforme data atual
+current_path = f"{TRUSTED_PATH}/year={year_val}/month={month_val}/day={day_val}/"
+
+# Escrever o dataframe como Parquet
+df_with_partitions.write.mode("overwrite").parquet(current_path)
+
